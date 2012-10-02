1e311e57679226f61145a2bb5d194bad5497e28f
Mani <sadhak001@gmail.com>
2012-10-02 22:18:09 +0100

    Reorganise project files, directory structure and refactor core tests

--- mani/openJDKProductivityTool/src/org/ljc/adoptojdk/Others/NotAFullyQualifiedClassNameException.java
+++
@@ -1,7 +0,0 @@
-package org.ljc.adoptojdk.Others;
-
-public class NotAFullyQualifiedClassNameException extends Exception {
-	public NotAFullyQualifiedClassNameException(String message) {
-		super(message);
-	}
-}
